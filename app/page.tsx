0215b0db26c9d64bc58b547d85ea68d560368d1a
Rabie Menad <rabie.menad@ideine.fr>
2023-10-28 15:15:47 +0200

    Chapter 2: CSS Styling 🎨

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { NextPage } from 'next';
 import AcmeLogo from '@/app/ui/acme-logo';
 
 import Link from 'next/link';
-export default function Page() {
+export const Page: NextPage = () => {
   return (
     <main className='flex min-h-screen flex-col p-6'>
       <div className='flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52'>
@@ -10,12 +11,13 @@ export default function Page() {
       <div className='mt-4 flex grow flex-col gap-4 md:flex-row'>
         <div className='flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20'>
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> This is the example for the
             <a href='https://nextjs.org/learn/' className='text-blue-500'>
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
           </p>
+          <div className='h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent' />
           <Link
             href='/login'
             className='flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base'
@@ -29,4 +31,6 @@ export default function Page() {
       </div>
     </main>
   );
-}
+};
+
+export default Page;
